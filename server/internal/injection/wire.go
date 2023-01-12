b59c05f5727076117bde6e1c4934604e655e9218
kenji <kenjix12x22@gmail.com>
2023-01-13 02:17:52 +0900

    create wire_gen.go

--- server/internal/injection/wire.go
+++ server/internal/injection/wire.go
@@ -1,12 +1,26 @@
 package injection
 
 import (
+	"github.com/kenji-kk/mucom-go/server/internal/interface/handler"
 	"github.com/kenji-kk/mucom-go/server/internal/usecase"
+	"github.com/kenji-kk/mucom-go/server/internal/repository"
+	
 
 	"github.com/google/wire"
 )
 
-func InitializeEvent() usecase.AuthUsecase {
-		wire.Build()
-    return usecase.WauthUsecase{}
+func InitializeHandlers() handler.Handlers {
+		wire.Build(
+			// handler
+			handler.NewHandlers,
+			handler.NewAuthHandler,
+
+			// usecase
+			usecase.NewAuthUsecase,
+
+			// repository
+			repository.NewAuthRepository,
+
+		)
+    return nil
 }
