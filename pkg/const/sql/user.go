1965e70faa42e16fca8a6a8bbe8c5ecbd10446e6
kenji <kenjix12x22@gmail.com>
2023-01-18 01:40:38 +0900

    create createUserQuery

---
+++ pkg/const/sql/user.go
@@ -0,0 +1,7 @@
+package sql
+
+const (
+	createUserQuery = `INSERT INTO users (user_name, email, hashed_password, created_at, updated_at)
+											VALUES ($1, $2, $3, now(), now()) 
+						          RETURNING *`
+	)
