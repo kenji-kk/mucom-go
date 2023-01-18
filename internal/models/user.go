2ead223bcb800dadfc89c05d80a96d021cf99645
kenji <kenjix12x22@gmail.com>
2023-01-18 22:24:15 +0900

    modify user-model

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,12 +6,12 @@ import (
 )
 
 type User struct {
-	Id        uuid.UUID		 
-	UserName  string `binding:"required,min=5,max=30"`
-	Password  string `binding:"required,min=6,max=30"`
-	Email     string `binding:"required,min=5,max=100"`
-	CreatedAt time.Time 
-	UpdatedAt time.Time 
-	Salt      []byte `json:"-"`
-	HashedPassword []byte `json:"-"`
+	Id        uuid.UUID    `json:"id" db:"id" validate:"omitempty"`
+	UserName  string       `json:"userName" db:"user_name binding:"required,min=5,max=30"`
+	Password  string       `json:"password" binding:"required,min=5,max=30"`
+	Email     string       `json:"email" db:"email" binding:"required,min=5,max=100"`
+	CreatedAt time.Time    `json:"createdAt,omitempty" db:"created_at"`
+	UpdatedAt time.Time    `json:"updatedAt,omitempty" db:"updated_at"`
+	Salt      []byte       `json:"-"`
+	HashedPassword []byte  `json:"-"`
 }
