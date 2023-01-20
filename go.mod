1a670ae1d9558396af3880ac32ea14baa0bc4a18
kenji <kenjix12x22@gmail.com>
2023-01-20 20:08:00 +0900

    add creating JWS

--- go.mod
+++ go.mod
@@ -34,7 +34,7 @@ require (
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/usecase/authUsecase.go
+++ internal/usecase/authUsecase.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"time"
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/kenji-kk/mucom-go/internal/repository"
 	"github.com/kenji-kk/mucom-go/internal/models"
@@ -9,7 +11,7 @@ import (
 
 type AuthUsecase interface {
 	Hello() string
-	CreateUser(context.Context, *models.User) (*models.User, error)
+	CreateUser(context.Context, *models.User) (*models.User, string, error)
 }
 
 type authUsecase struct {
@@ -24,6 +26,35 @@ func (usAuth *authUsecase) Hello() string {
 	return usAuth.reAuth.Hello()
 }
 
-func (usAuth *authUsecase) CreateUser(ctx context.Context, user *models.User) (*models.User, error) {
-	return usAuth.reAuth.CreateUser(ctx, user)
+func (usAuth *authUsecase) CreateUser(ctx context.Context, user *models.User) (*models.User, string, error) {
+	createdUser, err := usAuth.reAuth.CreateUser(ctx, user)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// passwordの値を空白にする
+	createdUser.Password = ""
+
+	// JWTの生成
+	jws := createJWT(createdUser.Id.String())
+
+	return createdUser, jws, err
+	
+
+}
+
+func createJWT(userID string) string{
+	// Claimsオブジェクト作成
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	// ヘッダーとペイロード作成
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// トークンに署名を付与
+	jws, _ := token.SignedString([]byte("SECRET_KEY"))
+
+	return jws
 }
